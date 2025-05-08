70531c6cce2e2f766e9b2976f32457eb5e4713cd
Laura <l.cooper.5@warwick.ac.uk>
2025-05-08 17:14:16 +0100

    don't ignore sldy directory, initial commit for testing .sld and .sldy files

---
+++ tests/test_modularPipeline.m
@@ -0,0 +1,26 @@
+% File: testmodularPipeline.m
+classdef test_modularPipeline < matlab.unittest.TestCase
+    methods (Test)
+        function testPipelineWithSld(testCase)
+            thisFile = mfilename('fullpath');
+            [thisDir, ~, ~] = fileparts(thisFile);
+            psfPath = fullfile(thisDir,'data', 'PSFs');
+            inputPath = fullfile(thisDir, 'data', 'Inputs', 'sld');
+            % Call the function being tested
+            modularPipeline(psfPath, inputPath); 
+            
+            % Assert that the result is correct
+            %testCase.verifyEqual(result, 5);
+        end
+        
+        function testPipelineWithSldy(testCase)
+            thisFile = mfilename('fullpath');
+            [thisDir, ~, ~] = fileparts(thisFile);
+            psfPath = fullfile(thisDir,'data', 'PSFs');
+            inputPath = fullfile(thisDir, 'data', 'Inputs', 'sldy');
+            % Simulate calling the function using those folders
+            modularPipeline(psfPath, inputPath); % assuming you modify the function to accept inputs
+            % Add assertions here
+        end
+    end
+end

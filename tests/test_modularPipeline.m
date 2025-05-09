fd8df64b88563fd47cf6517ad0abac7cbd7ed90c
Laura <l.cooper.5@warwick.ac.uk>
2025-05-09 14:57:12 +0100

    list expected files

--- tests/test_modularPipeline.m
+++ tests/test_modularPipeline.m
@@ -8,7 +8,12 @@ classdef test_modularPipeline < matlab.unittest.TestCase
             inputPath = fullfile(thisDir, 'data', 'Inputs', 'sld');
             % Call the function being tested
             modularPipeline(psfPath, inputPath); 
-            
+            expected_files = {
+                'example-data-2C-2T-3Z-1Series_Capture1_0point5_zSpacing_decondeskew.tif'
+                'example-data-2C-2T-3Z-1Series_Capture1_0point5_zSpacing_decondeskew_MAX.tif'
+                'example-data-2C-2T-3Z-1Series_Capture1_0point5_zSpacing_deskew.tif'
+                'example-data-2C-2T-3Z-1Series_Capture1_0point5_zSpacing_deskew_MAX.tif'
+               };
             % Assert that the result is correct
             %testCase.verifyEqual(result, 5);
         end
@@ -20,6 +25,12 @@ classdef test_modularPipeline < matlab.unittest.TestCase
             inputPath = fullfile(thisDir, 'data', 'Inputs', 'sldy');
             % Simulate calling the function using those folders
             modularPipeline(psfPath, inputPath); % assuming you modify the function to accept inputs
+            expected_files = {
+                'example-data-2C-2T-3Z-1Series_Capture_1_decondeskew.tif'
+                'example-data-2C-2T-3Z-1Series_Capture_1_decondeskew_MAX.tif'
+                'example-data-2C-2T-3Z-1Series_Capture_1_deskew.tif'
+                'example-data-2C-2T-3Z-1Series_Capture_1_deskew_MAX.tif'
+               };
             % Add assertions here
         end
     end
